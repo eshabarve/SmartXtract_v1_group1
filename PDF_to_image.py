f58a586cca7680d323820254bfca9aa867258d0b
esha.barve <eshabarve.24.eb@gmail.com>
2025-09-23 13:24:43 +0530

    updated pdftoimg file with changes

---
+++ PDF_to_image.py
@@ -0,0 +1,14 @@
+
+def pdf_to_images(pdf_path):
+   
+    from pdf2image import convert_from_path  # import inside function
+
+    images = convert_from_path(pdf_path, poppler_path=poppler_path, use_pdftocairo=True)
+    output_files = []
+
+    for i, img in enumerate(images):
+        filename = f"{output_prefix}_{i+1}.png"
+        img.save(filename, "PNG")
+        output_files.append(filename)
+
+    return output_files
